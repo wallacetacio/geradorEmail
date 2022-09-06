ed8e54e619e8f1909d0b996f774da8e01c88da6b
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-06 14:11:35 -0300

    Adiciona solução ao exercicio Gerador de E-mail

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+    string nomeCompleto, primeiroNome,  dominio = "@fatec.sp.gov.br";
+    int pos1;
+
+    Console.Write("Digite seu nome completo: ");
+    nomeCompleto = Console.ReadLine()
+    .Replace("das","").Replace("dos","")
+    .Replace("da","").Replace("do","")
+    .Replace("'","").Replace("de","")
+    .Replace("á","a").Replace("â","a")
+    .Replace("é","e").Replace("ê","e")
+    .Replace("í","i").Replace("î","i")
+    .Replace("ó","o").Replace("ô","o")
+    .Replace("ú","u").Replace("û","u");
+    
+pos1 = nomeCompleto.IndexOf(" ");
+
+primeiroNome = nomeCompleto.Substring(0,pos1);
+
+Console.WriteLine("E-mail institucional: " + primeiroNome.Trim().ToLower() + "." + dominio);
